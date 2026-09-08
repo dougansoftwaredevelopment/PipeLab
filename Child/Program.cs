f89596ffd87bf19e4617b4fe5edc1ea93b0c02fb
sean <svoogan@hotmail.com>
2026-09-08 15:54:40 -0400

    Stage 1

--- Child/Program.cs
+++ Child/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace Child;
+﻿using System.IO.Pipes;
+
+namespace Child;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Bootstrapping Child Pipe process...");
+        //Instantiate the client
+        using var client = new AnonymousPipeClientStream(PipeDirection.In, args[0]);
+        using var reader = new StreamReader(client);
+
+        string? line;
+        while ((line = reader.ReadLine()) is not null)
+            Console.WriteLine($"[child] got: {line}");
+            
+        Console.WriteLine("[Child] pipe closed, exiting");
     }
 }
--- Host/Program.cs
+++ Host/Program.cs
@@ -1,9 +1,48 @@
-﻿namespace Host;
+﻿using System.Diagnostics;
+using System.IO.Pipes;
+
+namespace Host;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
+        string childPath = Path.GetFullPath(Path.Combine(
+            AppContext.BaseDirectory,
+            "..", "..", "..", "..",
+            "Child", "bin", "Debug", "net10.0", "Child.exe"));
+
+        if (!File.Exists(childPath))
+            throw new FileNotFoundException($"child not built: {childPath}");
+        
+        
+        //create the pipe
+        using var server = new AnonymousPipeServerStream(PipeDirection.Out, HandleInheritability.Inheritable);//instantiate the pipe from the win32 and system.IO.Pipes bcl
+        var clientHandle = server.GetClientHandleAsString(); // need to grab the handle here
+        Console.WriteLine($"[host] handling child handle {clientHandle}"); // dump status to stdout
+
+        var psi = new ProcessStartInfo
+        {
+            FileName = childPath,
+            UseShellExecute = false
+        };
+        psi.ArgumentList.Add(clientHandle);
+
+        using var child = Process.Start(psi)!;
+        
+        
+        server.DisposeLocalCopyOfClientHandle();
+
+        using (var writer = new StreamWriter(server) {AutoFlush = true})
+        {
+            writer.WriteLine("Hello from host");
+            writer.WriteLine("line two");
+            writer.WriteLine("line three");
+        }
+
+        child.WaitForExit();
+        Console.WriteLine($"[host] child exited with {child.ExitCode}");
+
     }
 }
